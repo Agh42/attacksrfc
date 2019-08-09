486b9a6946a85456bd8245793a65a70f37e81112
Agh42 <bitbucket@koderman.de>
2019-08-09 23:14:02 +0200

    Fixed typo in App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default class App extends Component {
                           </div>
                         )} />
                 </Switch>
-            </Route>
+            </Router>
     );
   }
 }
