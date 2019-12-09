385625c7ec09a87676f41ca435a742aed95ce9b3
Alex K <bitbucket@koderman.de>
2019-12-09 07:42:33 +0100

    Fix test import

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Client from './Scripts/CpeClient.js'
+import Client from './Gateways/CpeClient.js'
 
-jest.mock('./Scripts/CpeClient.js');
+jest.mock('./Gateways/CpeClient.js');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
