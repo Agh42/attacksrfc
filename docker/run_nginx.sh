723c36dc53351cb0ef7625d48aabd21f1e14c910
Alexander Koderman <github@koderman.de>
2020-01-29 13:26:22 +0100

    Changed username.

--- docker/run_nginx.sh
+++ docker/run_nginx.sh
@@ -2,5 +2,5 @@
 # Use NGINX to test static hosting:
 #
 
-docker run --name attacksrfc-nginx -v /home/akoderman/git/attacksurface/build:/usr/share/nginx/html:ro -d -p 8081:80 nginx
+docker run --name attacksrfc-nginx -v /home/<user>/git/attacksurface/build:/usr/share/nginx/html:ro -d -p 8081:80 nginx
 
