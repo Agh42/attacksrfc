1bc45ac5430c669be4292bf267bf5b372578d0b4
Agh42 <bitbucket@koderman.de>
2021-02-17 23:26:26 +0100

    Changes to homepage text

--- src/Pages/AttackSrfcPage.js
+++ src/Pages/AttackSrfcPage.js
@@ -1170,6 +1170,7 @@ class AttackSrfcPage extends Component {
                 <div class="ui horizontal  small divided link list">
                 <a class="item" href="homepage.html">Home</a>
                 <a class="item" href="legal.html">Legal Notice and Contact</a>
+                <a class="item" href="/register">Register / Pricing</a>
                 <a class="item" target="_blank" rel="noopener noreferrer" href="https://github.com/Agh42/CSTOOL_io"> Source Code</a>
                 <a class="item" target="_blank" rel="noopener noreferrer" href="https://github.com/Agh42/attacksrfc/issues">Report issues</a>
                 <a class="item" target="_blank" rel="noopener noreferrer" href="https://stats.uptimerobot.com/RMwRDtvPLw">Site status</a>
